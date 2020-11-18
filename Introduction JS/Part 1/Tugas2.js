f76f1a811568ac64bf809d318ab83d70706f80f1
Andrawan Erlang Pradana <73692809+andrawanerlang1@users.noreply.github.com>
2020-11-18 13:59:09 +0700

    Update Tugas2.js

--- Introduction JS/Part 1/Tugas2.js
+++ Introduction JS/Part 1/Tugas2.js
@@ -1,16 +1,24 @@
-const mtk = 80
-const bahasaIndonesia = 50
-const bahasaInggris = 89
-const ipa = 69
-
-//validasi
-if (mtk==null || bahasaIndonesia==null || bahasaInggris==null || ipa==null){
-    console.log("Masukkan semua nilai input!");
-}
+const mtk = 90
+const bahasaIndonesia = 90
+const bahasaInggris = 80
+const ipa = 80
 
 const rataRata = (mtk+bahasaIndonesia+bahasaInggris+ipa)/4
 
-if(rataRata >= 90 && rataRata <= 100){
+//validasi null
+if (mtk==null || bahasaIndonesia==null || bahasaInggris==null || ipa==null){
+    console.log("Masukkan semua nilai input!");
+}
+//validasi input number
+else if (typeof(mtk)!='number'||typeof(bahasaIndonesia)!='number'||typeof(bahasaInggris)!='number'||typeof(ipa)!='number'){
+    console.log("Masukkan input berupa number!");
+}
+//validasi range input
+else if ((mtk>100||mtk<0)||(bahasaIndonesia>100||bahasaIndonesia<0)||(bahasaInggris>100||bahasaInggris<0)||(ipa>100||ipa<0)){
+    console.log("Masukkan nilai dengan range 0 sampai 100!");
+}
+//percabangan hasil rata-rata
+else if(rataRata >= 90 && rataRata <= 100){
     console.log(`Rata-rata = ${rataRata}\nGrade = A`)
 }
 else if (rataRata >= 80 && rataRata <= 89){
